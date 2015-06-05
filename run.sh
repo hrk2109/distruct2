25e0b957a3232685848ec52b350b6c5f908c3147
Vikram Chhatre <vikram@ip0af52faa.int.uvm.edu>
2015-06-05 14:26:44 -0400

    distruct2 first commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python distruct2.py -K 3 --input=testoutput_simple --output='TestK3_user.png' --popfile=pop --title='fastStructure Test Data K=3 Simple Prior 3 Pops 200 Inds (distruct2.py)'
