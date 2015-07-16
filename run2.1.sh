548cc57c5ac5f1ed9e69d30ee66f526b95aa1125
cryptic0 <crypticlineage@gmail.com>
2015-07-15 22:22:31 -0400

    Second commit

---
+++ run2.1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python distruct2.1.py -K 3 --input=testoutput_simple --output='TestK3_Poporder.pdf' --popfile=pop --title='fastStructure Test Data K=3 Simple Prior 3 Pops 200 Inds (distruct2.1.py)'
